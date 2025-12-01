a608ba2fdf3cb3cd3b1633a934c5753a1cc0384d
ElijahAlquino <enalquino@mcm.edu.ph>
2025-12-01 23:25:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+
 const toggleButton = document.getElementById('theme-toggle');
 const body = document.body;
+
 toggleButton.addEventListener('click', () => {
     body.classList.toggle('dark');
     body.classList.toggle('red');
+});
+
+
+const fadeElements = document.querySelectorAll('section, header');
+
+window.addEventListener('load', () => {
+    fadeElements.forEach(el => {
+        el.classList.add('fade-in');
+        setTimeout(() => {
+            el.classList.add('show');
+        }, 150);
+    });
 });
