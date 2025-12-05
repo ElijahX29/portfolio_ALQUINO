09fd20d4fd53dc89245836654df0264e30171161
ElijahAlquino <enalquino@mcm.edu.ph>
2025-12-05 20:13:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,21 +6,20 @@ toggleButton.addEventListener('click', () => {
 });
 
 const fadeElements = document.querySelectorAll('section, header');
-
 window.addEventListener('load', () => {
     fadeElements.forEach(el => {
         el.classList.add('fade-in');
-        setTimeout(() => {
-            el.classList.add('show');
-        }, 150);
+        setTimeout(() => { el.classList.add('show'); }, 150);
     });
 });
 
 document.getElementById("song-btn").addEventListener("click", () => {
-    window.open("https://open.spotify.com/track/7JXZq0JgG2zTrSOAgY8VMC?si=c585eb23d54e41ac", "_blank");
+    window.open("https://open.spotify.com/track/7JXZq0JgG2zTrSOAgY8VMC?si=36a913f1e66040ea", "_blank");
 });
 
-
+document.getElementById("profile-btn").addEventListener("click", () => {
+    window.open("https://open.spotify.com/user/YOUR-USER-ID-HERE", "_blank");
+});
 
 document.getElementById("website-btn").addEventListener("click", () => {
     window.open("https://elijahx29.github.io/Alquino_CSS/", "_blank");
