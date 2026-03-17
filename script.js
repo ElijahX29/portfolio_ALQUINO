0ebeb41d67e08e28681fa30a32a1dfd62635a55c
ElijahAlquino <enalquino@mcm.edu.ph>
2026-03-18 00:48:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ toggleButton.addEventListener('click', () => {
   }
 });
 
+// Fade-in animation
 const fadeElements = document.querySelectorAll('section, header');
 window.addEventListener('load', () => {
   fadeElements.forEach(el => {
@@ -32,9 +33,25 @@ window.addEventListener('load', () => {
   });
 });
 
+// Spotify button
 document.getElementById("profile-btn").addEventListener("click", () => {
   window.open(
     "https://open.spotify.com/user/xcm3zr7hdiyjh2cy3867l9o3m",
+    "_blank"
+  );
+});
+
+// ✅ NEW BUTTONS
+document.getElementById("yt-btn").addEventListener("click", () => {
+  window.open(
+    "https://youtu.be/uUOmJX4GdSk?si=cDiZyiAucL96BHA2",
+    "_blank"
+  );
+});
+
+document.getElementById("report-btn").addEventListener("click", () => {
+  window.open(
+    "https://malayancollegesmindanaoo365-my.sharepoint.com/:f:/g/personal/enalquino_mcm_edu_ph/IgAko0KLzH3gRZvMl1iOO6KYAZoNjmumQYhL3MV8MLwjrCM?e=bGI6yH",
     "_blank"
   );
 });
